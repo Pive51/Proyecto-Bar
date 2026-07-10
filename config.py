21faeb1a40f13cda75f5236cd32d5bc1f2df23e4
Pive Arevalo <156113136+Pive51@users.noreply.github.com>
2026-07-10 01:56:44 -0500

    Eliminar credenciales del proyecto

--- config.py
+++ config.py
@@ -1,17 +1,9 @@
 # config.py
+import os
 
-# Credenciales de tu nueva base de datos en Supabase (Región: Canadá)
-DB_USER = "postgres.ofscbbmyfmexyzrguyfh"
-DB_PASS = "ProyectoFinal01"
-DB_HOST = "aws-0-ca-central-1.pooler.supabase.com"
-DB_PORT = "5432"
-DB_NAME = "postgres"
+CONNECTION_STRING = os.getenv("DATABASE_URL")
 
-# Tu nueva cadena de conexión optimizada para PostgreSQL (psycopg2)
-CONNECTION_STRING = (
-    f"dbname='{DB_NAME}' "
-    f"user='{DB_USER}' "
-    f"password='{DB_PASS}' "
-    f"host='{DB_HOST}' "
-    f"port='{DB_PORT}'"
-)
+if not CONNECTION_STRING:
+    raise ValueError(
+        "La variable de entorno DATABASE_URL no está configurada."
+    )
